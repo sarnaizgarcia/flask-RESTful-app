73a5359253d2ac6c6c31cdd1a2fa8809ae2576e2
Silvia <sarnaizgarcia@gmail.com>
2019-11-23 17:05:01 +0100

    authentication and logging

--- code/app.py
+++ code/app.py
@@ -1,18 +1,22 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'holita'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
-        # for item in items:
-        #     if item['name'] == name:
-        #         return item
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
